3aa277e893aaa1d2ac814a586902e5385f4b265e
gabbyca <gabbycamm06@gmail.com>
2023-10-31 13:08:48 -0400

    commit

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,5 +1,13 @@
 package frc.robot.subsystems;
 
-public class DriveSubsystem {
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
     
+    Talon b  
+
+    public DriveSubsystem(){}
+
+
 }
